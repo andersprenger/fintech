c59651b70da2a57c0139f57516e3e676ac4d44de
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2021-09-16 18:10:03 -0300

    rm commented code

--- src/fintech/util/OperationsHeap.java
+++ src/fintech/util/OperationsHeap.java
@@ -65,44 +65,4 @@ public abstract class OperationsHeap {
     public int size() {
         return used;
     }
-
- /* magic print stuff... idk how to make it work with generic types...
-
-    private int len(int a) {
-        int res = 0;
-        while (a > 0) {
-            res++;
-            a /= 10;
-        }
-        return res;
-    }
-
-    private void print(int b, int elem, int sp) {
-        int i, j;
-
-        System.out.println("");
-        for (j = 0; j < used; j++) System.out.print(v[j] + " ");
-        System.out.println("");
-
-        while (true) {
-            for (j = 0; j <= sp / 2; j++) System.out.print(" ");
-            for (i = b; i < b + elem; i++) {
-                if (i == used) return;
-                int aux = len(v[i]);
-                System.out.print(v[i]);
-                for (j = 0; j < sp - aux; j++) System.out.print(" ");
-            }
-            System.out.println("");
-            b = b + elem;
-            elem = 2 * elem;
-            sp = sp / 2;
-        }
-    }
-
-    public void print() {
-        System.out.println("");
-        print(0, 1, 64);
-        System.out.println("");
-    }
-*/
 }
